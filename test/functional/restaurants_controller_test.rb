f831f1d10a04c5a23d0e07789812c16c3ade8795
Piotr Kowalski <piotr.kowalski@me.com>
2013-09-20 15:21:40 +0200

    Updated tests.

--- test/functional/restaurants_controller_test.rb
+++ test/functional/restaurants_controller_test.rb
@@ -6,8 +6,23 @@ class RestaurantsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "index should contain exactly 2 pizzerias" do
+    get :index
+    assert_select 'ul' do |elements|
+      assert_select elements.first, "li", 2
+    end
+  end
+
   test "should get show" do
     get :show, id: restaurants(:place)
     assert_response :success
   end
+
+  test "show should display 2 pizzerias" do
+    get :show, id: restaurants(:place)
+    assert_select 'div table' do |elements|
+      assert_select elements.first, "tr", restaurants(:place).pizzas.count + 1
+    end
+  end
+
 end
